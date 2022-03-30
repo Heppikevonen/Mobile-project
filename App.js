05f9ed865f7e08c03bb0cb5e22b4c523a1a65d5a
jonivalo <jonivirvalo@gmail.com>
2022-03-30 11:57:51 +0300

    audioplayer

--- App.js
+++ App.js
@@ -1,20 +1,181 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { StyleSheet, TouchableOpacity, View, Image, Text } from 'react-native'
+import { Ionicons } from '@expo/vector-icons'
+import { Audio } from 'expo-av'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const runnerRadioPlaylist = [
+	{
+		title: 'Add Ttitle',
+		author: 'Add Author',
+		uri:
+			'https://firebasestorage.googleapis.com/v0/b/testi-f9856.appspot.com/o/naytappa.mp3?alt=media&token=78f8faed-f1c6-4083-8462-85f892cbee5c',
+		imageSource: 'https://firebasestorage.googleapis.com/v0/b/testi-f9856.appspot.com/o/Screenshot%202022-03-30%20000803.png?alt=media&token=ca9bb44b-2b14-411b-9eed-b5fab259b5c8'
+	}
+]
+
+export default class App extends React.Component {
+	state = {
+		isPlaying: false,
+		playbackInstance: null,
+		currentIndex: 0,
+		volume: 1.0,
+		isBuffering: true
+	}
+
+	async componentDidMount() {
+		try {
+			await Audio.setAudioModeAsync({
+				allowsRecordingIOS: false,
+				interruptionModeIOS: Audio.INTERRUPTION_MODE_IOS_DO_NOT_MIX,
+				playsInSilentModeIOS: true,
+				interruptionModeAndroid: Audio.INTERRUPTION_MODE_ANDROID_DO_NOT_MIX,
+				shouldDuckAndroid: true,
+				staysActiveInBackground: true,
+				playThroughEarpieceAndroid: true
+			})
+
+			this.loadAudio()
+		} catch (e) {
+			console.log(e)
+		}
+	}
+
+	async loadAudio() {
+		const { currentIndex, isPlaying, volume } = this.state
+
+		try {
+			const playbackInstance = new Audio.Sound()
+			const source = {
+				uri: runnerRadioPlaylist[currentIndex].uri
+			}
+
+			const status = {
+				shouldPlay: isPlaying,
+				volume: volume
+			}
+
+			playbackInstance.setOnPlaybackStatusUpdate(this.onPlaybackStatusUpdate)
+			await playbackInstance.loadAsync(source, status, false)
+			this.setState({
+				playbackInstance
+			})
+		} catch (e) {
+			console.log(e)
+		}
+	}
+
+	onPlaybackStatusUpdate = status => {
+		this.setState({
+			isBuffering: status.isBuffering
+		})
+	}
+
+	handlePlayPause = async () => {
+		const { isPlaying, playbackInstance } = this.state
+		isPlaying ? await playbackInstance.pauseAsync() : await playbackInstance.playAsync()
+
+		this.setState({
+			isPlaying: !isPlaying
+		})
+	}
+
+	handlePreviousTrack = async () => {
+		let { playbackInstance, currentIndex } = this.state
+		if (playbackInstance) {
+			await playbackInstance.unloadAsync()
+			currentIndex < runnerRadioPlaylist.length - 1 ? (currentIndex -= 1) : (currentIndex = 0)
+			this.setState({
+				currentIndex
+			})
+			this.loadAudio()
+		}
+	}
+
+	handleNextTrack = async () => {
+		let { playbackInstance, currentIndex } = this.state
+		if (playbackInstance) {
+			await playbackInstance.unloadAsync()
+			currentIndex < runnerRadioPlaylist.length - 1 ? (currentIndex += 1) : (currentIndex = 0)
+			this.setState({
+				currentIndex
+			})
+			this.loadAudio()
+		}
+	}
+
+	renderFileInfo() {
+		const { playbackInstance, currentIndex } = this.state
+		return playbackInstance ? (
+			<View style={styles.trackInfo}>
+				<Text style={[styles.trackInfoText, styles.largeText]}>
+					{runnerRadioPlaylist[currentIndex].title}
+				</Text>
+				<Text style={[styles.trackInfoText, styles.smallText]}>
+					{runnerRadioPlaylist[currentIndex].author}
+				</Text>
+			</View>
+		) : null
+	}
+
+	render() {
+		return (
+			<View style={styles.container}>
+				<Image
+					style={styles.albumCover}
+					source={{ uri: 'https://firebasestorage.googleapis.com/v0/b/testi-f9856.appspot.com/o/Screenshot%202022-03-30%20000803.png?alt=media&token=ca9bb44b-2b14-411b-9eed-b5fab259b5c8' }}
+				/>
+				<View style={styles.controls}>
+					<TouchableOpacity style={styles.control} onPress={this.handlePreviousTrack}>
+						<Ionicons name="arrow-back-circle-outline" size={48} color='#444' />
+					</TouchableOpacity>
+					<TouchableOpacity style={styles.control} onPress={this.handlePlayPause}>
+						{this.state.isPlaying ? (
+							<Ionicons name="pause-circle-outline" size={48} color='#444' />
+						) : (
+							<Ionicons name="play-circle-outline" size={48} color='#444' />
+						)}
+					</TouchableOpacity>
+					<TouchableOpacity style={styles.control} onPress={this.handleNextTrack}>
+						<Ionicons name="arrow-forward-circle-outline" size={48} color='#444' />
+					</TouchableOpacity>
+				</View>
+				{this.renderFileInfo()}
+			</View>
+		)
+	}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center'
+	},
+	albumCover: {
+		width: 250,
+		height: 250
+	},
+	trackInfo: {
+		padding: 40,
+		backgroundColor: '#fff'
+	},
+
+	trackInfoText: {
+		textAlign: 'center',
+		flexWrap: 'wrap',
+		color: '#550088'
+	},
+	largeText: {
+		fontSize: 22
+	},
+	smallText: {
+		fontSize: 16
+	},
+	control: {
+		margin: 20
+	},
+	controls: {
+		flexDirection: 'row'
+	}
+})
