30193a5a785b8c5544a3be1dc83042bf8abe23c7
jonivalo <jonivirvalo@gmail.com>
2022-04-06 03:43:37 +0300

    Styles folder modified, new constants folder created, PodcastPlaylist file created for RSS data experiments, RadioPlayer not yet finished, icons added to Navigator

--- styles/styles.js
+++ styles/styles.js
@@ -5,8 +5,6 @@ export default StyleSheet.create({
   container: {
     paddingTop: Constants.statusBarHeight,
     flex: 1,
-	//alignItems: 'center',
-	//justifyContent: 'center',
     marginLeft: 16,
     marginRight: 16,
   },
@@ -14,7 +12,7 @@ export default StyleSheet.create({
     width: 150, 
     height: 150,
 	marginBottom: 6,
-	
+
 	
 
   },
@@ -23,48 +21,85 @@ export default StyleSheet.create({
     fontWeight: 'bold',
 	width: 150,
 	marginBottom: 24,
+  },
+  
+  containerPlayer: {
+		flex: 1,
+		flexDirection: 'column',
+		justifyContent: 'space-between',
+		alignItems: 'center',
+		alignSelf: 'stretch',
+		backgroundColor: '#f94c94',
+	},
+	portraitContainer: {
+		marginTop: 0,
+	},
+	portrait: {
+		height: 300,
+		width: 450,
+	},
+	detailsContainer: {
+		height: 150,
+		marginTop: 10,
+		alignItems: 'center',
+	},
+	playbackContainer: {
+		flex: 1,
+		flexDirection: 'column',
+		justifyContent: 'space-between',
+		alignItems: 'center',
+		alignSelf: 'stretch',
+	},
+	playbackSlider: {
+		alignSelf: 'stretch',
+		marginLeft: 10,
+		marginRight: 10,
+	},
+	text: {
+		fontSize: 16,
+		minHeight: 16,
+		marginBottom: 20,
+		justifyContent: 'center',
+		alignItems: 'center',
+		color: '#ffffff'
+	},
+	sliderThumb: {
+		width: 12,
+		height: 12,
+		backgroundColor: '#f62976',
+		borderRadius: 10 / 2,
+		shadowColor: 'red',
+		shadowOffset: {
+			width: 0,
+			height: 0
+		},
+		shadowRadius: 2,
+		shadowOpacity: 1,
+	},
+	sliderTrack: {
+		height: 2,
+		backgroundColor: '#D3D3D3',
+	},
+	buttonsContainerBase: {
+		flex: 1,
+		flexDirection: 'row',
+		alignItems: 'center',
+		justifyContent: 'space-between',
+		marginTop: 10
+	},
+	buttonsContainerTopRow: {
+		maxHeight: 40,
+		minWidth: DEVICE_WIDTH / 2.0,
+		maxWidth: DEVICE_WIDTH / 2.0,
+	},
+	buttonsContainerMiddleRow: {
+		maxHeight: 40,
+		alignSelf: 'stretch',
+		paddingRight: 20,
+	},
 	
-  },
-  flex: {
-    flexDirection: "row", 
-    marginTop: 20,
-    height: 60,
-  },
-  containerRadio: {
-	flex: 1,
-	backgroundColor: '#fff',
-	alignItems: 'center',
-	justifyContent: 'center'
-  },
-  radioCover: {
-	width: 250,
-	height: 250
-  },
-  trackInfo: {
-	padding: 40,
-	backgroundColor: '#fff'
-  },
-  trackInfoText: {
-	textAlign: 'center',
-	flexWrap: 'wrap',
-	color: '#550088'
-  },
-  largeText: {
-	fontSize: 22
-  },
-  smallText: {
-	fontSize: 16
-  },
-  control: {
-  margin: 20
-  },
-  controls: {
-	flexDirection: 'row'
-  },
-  progressBar: {
-	width: 350,
-    height: 40,
-    marginTop: 25,
-    flexDirection: 'row',
-  },
+	buttonsContainerBottomRow: {
+		alignSelf: 'stretch',
+	},
+	
 })
