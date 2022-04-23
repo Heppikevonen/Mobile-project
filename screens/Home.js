052e5a81728c45bd952230c798590705021cad87
David Werner <davidwerner2@gmx.de>
2022-04-23 19:54:43 +0300

    Added database connection to home screen

--- screens/Home.js
+++ screens/Home.js
@@ -1,36 +1,59 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import {Text, ScrollView, ImageBackground, View} from "react-native" 
 import { Provider as PaperProvider, useTheme, List, Divider, Button } from 'react-native-paper'
 import styles from '../styles/styles'
 import theme from '../styles/Theme'
+import { db, ROOT_REF_NEWS, ROOT_REF_UPCOMING_BROADCASTS, ROOT_REF_HEADER } from '../firebase/Config'
 
 export default function Home() {
     const { colors } = useTheme(theme);
     const [expanded, setExpanded] = React.useState(true);
+    const [news, setNews] = useState([]); 
+    const [upcomingBroadcasts, setUpcomingBroadcasts] = useState([]); 
+    const [header, setHeader] = useState([]); 
+
     const handlePress = () => setExpanded(!expanded);
 
     const image = { uri: "https://runner-radio.de/wp-content/uploads/2022/02/one-run-one-book.jpg" };
     const image2 = { uri: "https://runner-radio.de/wp-content/uploads/2022/02/strandlauf.jpg" };
 
+    useEffect(() => {
+      db.ref(ROOT_REF_NEWS).on('value', querySnapShot => {
+        let data = querySnapShot.val() ? querySnapShot.val() : {};
+        setNews(data);
+      }
+      );
+      db.ref(ROOT_REF_UPCOMING_BROADCASTS).on('value', querySnapShot => {
+        let data = querySnapShot.val() ? querySnapShot.val() : {};
+        setUpcomingBroadcasts(data);
+      }
+      );
+      db.ref(ROOT_REF_HEADER).on('value', querySnapShot => {
+        let data = querySnapShot.val() ? querySnapShot.val() : {};
+        setHeader(data);
+      }
+      );
+    }, []);
+
+  let keyNews = Object.keys(news);
+  let keyUpcomingBroadcasts = Object.keys(upcomingBroadcasts);
+  
   return (
       <PaperProvider theme={theme}>
           {/* <View style={styles.container}>  */}
-          
            <ImageBackground source={image} resizeMode="cover" style={styles.image}>
             <Text style={[styles.textImage, {color: colors.onPrimary}]}>
-                The broadcast is not live. {"\n"}
-                Next broadcast on 17th of April. 
+              {header.title1} {"\n"} 
+              {header.title2}
             </Text>
-            
             <View>
             <Button 
                 //icon="music" 
                 mode="contained" 
                 onPress={() => console.log('Pressed')}
-                disabled={false}
+                disabled={header.buttonActive}
                 uppercase={false}
                 style={styles.buttonSmall}
-                
                 >
                 Listen now
             </Button>
@@ -38,56 +61,48 @@ export default function Home() {
            </ImageBackground>
            <ScrollView>
         <List.Section title="Upcoming broadcasts" titleStyle={styles.textAccordion}>
-          <List.Accordion
-            titleNumberOfLines={3}
-            title="Sunday 17 April 2022  - 7:00 to 11:00"
-            description="Beach run"
-            descriptionNumberOfLines={5}
-            left={props => <List.Icon {...props} icon="beach" />}>
+        {keyUpcomingBroadcasts.length > 0 ? (
+          keyUpcomingBroadcasts.map(key => (
+            <List.Accordion
+            key={key}
+            id={key}
+            titleNumberOfLines={upcomingBroadcasts[key].titleNumberOfLines}
+            title={upcomingBroadcasts[key].title}
+            description={upcomingBroadcasts[key].description}
+            descriptionNumberOfLines={upcomingBroadcasts[key].descriptionNumberOfLines}
+            left={props => <List.Icon {...props} icon={upcomingBroadcasts[key].icon}/>}>
             <List.Item 
-                titleNumberOfLines={10} 
-                title="The perfect start to the day, whether you’re running, lying on the couch or having your first to fifth coffee/tea. Calm, relaxed music accompanies us for 45 minutes and inbetween we hear the sound of the sea again and again." />
-            
-          </List.Accordion>
-
-          <List.Accordion
-            titleNumberOfLines={3}
-            title="Sunday 24 April 2022 - 12:00 to 14:30 "
-            description="One Book, One Run"
-            descriptionNumberOfLines={5}
-            left={props => <List.Icon {...props} icon="book"/>}>
-            <List.Item 
-                titleNumberOfLines={10} 
-                title="The run that not only helps you in terms of sport. Suitable music for running and in between listening to the summary of an interesting book. 60 minutes just flew by." />
-            
-          </List.Accordion>
+                titleNumberOfLines={upcomingBroadcasts[key].itemNumberOfLines}
+                title={upcomingBroadcasts[key].detailedDescription} />
+            </List.Accordion>
+          ))
+        ) : (
+          <Text style={styles.infoText}>There are no news available right now</Text>
+        )}
           </List.Section>
 
-          <List.Section title="News" titleStyle={styles.textAccordion}>
-          <List.Accordion
-            titleNumberOfLines={3}
-            title="Help me out"
-            description="Which day do you prefere the podcast?"
-            descriptionNumberOfLines={5}
-            left={props => <List.Icon {...props} icon="help" />}>
+          <List.Section title="News" titleStyle={styles.textAccordion}>         
+          {keyNews.length > 0 ? (
+          keyNews.map(key => (
+            <List.Accordion
+            key={key}
+            id={key}
+            titleNumberOfLines={news[key].titleNumberOfLines}
+            title={news[key].title}
+            description={news[key].description}
+            descriptionNumberOfLines={news[key].descriptionNumberOfLines}
+            left={props => <List.Icon {...props} icon={news[key].icon}/>}
+            >
             <List.Item 
-                titleNumberOfLines={10} 
-                title="The perfect start to the day, whether you’re running, lying on the couch or having your first to fifth coffee/tea. Calm, relaxed music accompanies us for 45 minutes and inbetween we hear the sound of the sea again and again." />
-            
-          </List.Accordion>
+                titleNumberOfLines={news[key].itemNumberOfLines}
+                title={news[key].detailedDescription} />
+            </List.Accordion>
+          
+          ))
+        ) : (
+          <Text style={styles.infoText}>There are no news available right now</Text>
+        )}
 
-          <List.Accordion
-            titleNumberOfLines={3}
-            title="New podcast"
-            description="The Jordan B Peterson Podcast 239 is out now!"
-            descriptionNumberOfLines={5}
-            left={props => <List.Icon {...props} icon="podcast"/>}>
-            <List.Item 
-                titleNumberOfLines={10} 
-                title="The run that not only helps you in terms of sport. Suitable music for running and in between listening to the summary of an interesting book. 60 minutes just flew by." />
-            
-          </List.Accordion>
-    
           {/* <List.Accordion
             title="Controlled Accordion"
             left={props => <List.Icon {...props} icon="folder" />}
