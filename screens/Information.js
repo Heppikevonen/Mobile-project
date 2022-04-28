7da81b3295fb4dc4833b274464c78b872e8404e2
Marjan van Dijk <481699@student.saxion.nl>
2022-04-28 12:01:23 +0300

    styling for information screen

--- screens/Information.js
+++ screens/Information.js
@@ -16,25 +16,53 @@ export default function Information({ navigation }) {
   return (
     <PaperProvider theme={theme}>
       <ScrollView>
-        <View>
-          <Text style={styles.textAccordion}>About me</Text>
-          <Text style={styles.textMediumBody}>
-            My name is Thorsten and I have been a passionate runner for many
-            years. For me, running is not just a sport, but a way of life and
-            part of my identity. The idea for the runner radio came to me while
-            I was running.
+        <View style={{ margin: 16 }}>
+          <Text
+            style={[
+              styles.textAccordion,
+              { alignSelf: "center", paddingBottom: 4 },
+            ]}
+          >
+            About me
           </Text>
-          <Image source={require("../assets/images/AboutMe.png")} style={{width: 132, height: 132}}/>
+          <View style={styles.flexContainer}>
+            <Text style={[styles.textMediumBody, { flex: 3 }]}>
+              My name is Thorsten and I have been a passionate runner for many
+              years. For me, running is not just a sport, but a way of life and
+              part of my identity. The idea for the runner radio came to me
+              while I was running.
+            </Text>
+            <View style={{ flex: 1, padding: 4 }}>
+              <Image
+                source={require("../assets/images/AboutMe.png")}
+                style={{ width: 90, height: 90 }}
+              />
+            </View>
+          </View>
         </View>
         <View>
-          <Text style={styles.textAccordion}>Running formats</Text>
-          <View>
+          <Text
+            style={[
+              styles.textAccordion,
+              { alignSelf: "center", paddingBottom: 4 },
+            ]}
+          >
+            Running formats
+          </Text>
+          <View style={styles.runsContainer}>
             <ImageBackground
               source={require("../assets/images/VirtualRunningMeeting.png")}
-              style={styles.image}
+              style={styles.runsImg}
             >
-              <Text style={styles.textAccordion}>Virtual Running Meeting</Text>
-              <Text style={styles.textMediumBody}>
+              <Text
+                style={[
+                  styles.runsHeader,
+                  { width: 100, marginLeft: 3, marginTop: 58 },
+                ]}
+              >
+                Virtual Running Meeting
+              </Text>
+              <Text style={[styles.runsBody, { width: 200, marginTop: 15 }]}>
                 We get in the mood for 10 minutes in the “Virtual Starting
                 Block” before we start a 60 minute run together. Appropriate
                 music accompanies the different phases (warm-up, main phase,
@@ -42,13 +70,13 @@ export default function Information({ navigation }) {
               </Text>
             </ImageBackground>
           </View>
-          <View>
+          <View style={styles.runsContainer}>
             <ImageBackground
               source={require("../assets/images/BeachRun.png")}
-              style={styles.image}
+              style={styles.runsImg}
             >
-              <Text style={styles.textAccordion}>Beach Run</Text>
-              <Text style={styles.textMediumBody}>
+              <Text style={[styles.runsHeader, { margin: 30 }]}>Beach Run</Text>
+              <Text style={[styles.runsBody, { width: 180, marginTop: 10 }]}>
                 The perfect start to the day, whether you're running, lying on
                 the couch or having your first to fifth coffee/tea. Calm,
                 relaxed music accompanies us for 45 minutes and inbetween we
@@ -56,54 +84,79 @@ export default function Information({ navigation }) {
               </Text>
             </ImageBackground>
           </View>
-          <View>
+          <View style={styles.runsContainer}>
             <ImageBackground
               source={require("../assets/images/OneBookOneRun.png")}
-              style={styles.image}
+              style={styles.runsImg}
             >
-              <Text style={styles.textAccordion}>One Run, One Book</Text>
-              <Text style={styles.textMediumBody}>
+              <Text
+                style={[
+                  styles.runsHeader,
+                  { width: 80, marginLeft: 3, marginTop: 39 },
+                ]}
+              >
+                One Run, One Book
+              </Text>
+              <Text
+                style={[
+                  styles.runsBody,
+                  { width: 200, marginTop: 30, marginRight: 10 },
+                ]}
+              >
                 The run that not only helps you in terms of sport. Suitable
                 music for running and in between listening to the summary of an
                 interesting book. 60 minutes just flew by.
               </Text>
             </ImageBackground>
           </View>
-          <View>
+          <View style={styles.runsContainer}>
             <ImageBackground
               source={require("../assets/images/FeelTheBeat.png")}
-              style={styles.image}
+              style={styles.runsImg}
             >
-              <Text style={styles.textAccordion}>Feel the beat</Text>
-              <Text style={styles.textMediumBody}>
+              <Text style={[styles.runsHeader, { marginLeft: 10 }]}>
+                Feel The Beat
+              </Text>
+              <Text style={[styles.runsBody, { width: 180 }]}>
                 Just let the tempo of the songs guide you in this musical
-                driving game. But manage your strength weell during the 45
+                driving game. But manage your strength well during the 45
                 minutes, you don't know exactly how long the stress and rest
                 phases are.
               </Text>
             </ImageBackground>
           </View>
-          <View>
+          <View style={styles.runsContainer}>
             <ImageBackground
               source={require("../assets/images/LongRun.png")}
-              style={styles.image}
+              style={styles.runsImg}
             >
-              <Text style={styles.textAccordion}>Long run</Text>
-              <Text style={styles.textMediumBody}>
-                Here you can chat about various topics (not just about running),
-                also with interesting interview guests. Of course, motivating
-                music should not be missing during the long run. How long is
-                this? May vary, but definatly at least 1.5 hours.
+              <Text
+                style={[styles.runsHeader, { marginTop: 35, marginLeft: 10 }]}
+              >
+                Long Run
+              </Text>
+              <Text style={[styles.runsBody, { width: 130, marginRight: 105 }]}>
+                Here you can chat about various topics with interesting
+                interview guests. Of course, motivating music should not be
+                missing during the long run. How long is this? May vary, but at
+                least 1.5 hours.
               </Text>
             </ImageBackground>
           </View>
-          <View>
+          <View style={styles.runsContainer}>
             <ImageBackground
               source={require("../assets/images/SpecialOffer.png")}
-              style={styles.image}
+              style={styles.runsImg}
             >
-              <Text style={styles.textAccordion}>Special offers</Text>
-              <Text style={styles.textMediumBody}>
+              <Text style={[styles.runsHeader, { marginLeft: 10 }]}>
+                Special Offers
+              </Text>
+              <Text
+                style={[
+                  styles.runsBody,
+                  { width: 150, marginTop: 150, marginRight: 55 },
+                ]}
+              >
                 Can be anything: from a running party to surprise runs. As
                 diverse as running.
               </Text>
--- styles/styles.js
+++ styles/styles.js
@@ -1,6 +1,6 @@
-import { StyleSheet, Dimensions, StatusBar, Platform } from 'react-native'
-import Constants from 'expo-constants'
-import theme from './Theme'
+import { StyleSheet, Dimensions, StatusBar, Platform } from "react-native";
+import Constants from "expo-constants";
+import theme from "./Theme";
 
 export default StyleSheet.create({
   container: {
@@ -8,251 +8,290 @@ export default StyleSheet.create({
     flex: 1,
     marginLeft: 16,
     marginRight: 16,
-    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
   },
   tinyLogo: {
     width: 150,
     height: 150,
-    marginBottom: 6
+    marginBottom: 6,
   },
   podcastHeadline: {
     fontSize: 16,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     width: 150,
-    marginBottom: 24
+    marginBottom: 24,
   },
 
   containerPlayer: {
     flex: 1,
     //flexDirection: 'column',
-    justifyContent: 'space-between',
-    alignItems: 'center',
+    justifyContent: "space-between",
+    alignItems: "center",
     //alignSelf: 'stretch',
-    backgroundColor: 'white'
+    backgroundColor: "white",
   },
   imageContainer: {
     width: 300,
     height: 340,
-    marginBottom: 25
+    marginBottom: 25,
   },
   image: {
-    width: '100%',
-    height: '100%',
-    borderRadius: 15
+    width: "100%",
+    height: "100%",
+    borderRadius: 15,
   },
   detailsContainer: {
     height: 130,
     marginTop: 10,
-    alignItems: 'center'
+    alignItems: "center",
   },
   playbackContainer: {
     flex: 1,
-    flexDirection: 'column',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    alignSelf: 'stretch'
+    flexDirection: "column",
+    justifyContent: "space-between",
+    alignItems: "center",
+    alignSelf: "stretch",
   },
   playbackSlider: {
-    alignSelf: 'stretch',
+    alignSelf: "stretch",
     marginLeft: 10,
-    marginRight: 10
+    marginRight: 10,
   },
   timeStamp: {
     flex: 1,
-    width: '80%',
-    flexDirection: 'row',
-    justifyContent: 'space-between'
+    width: "80%",
+    flexDirection: "row",
+    justifyContent: "space-between",
   },
 
   text: {
     fontSize: 16,
     minHeight: 16,
     marginBottom: 20,
-    justifyContent: 'center',
-    alignItems: 'center',
-    color: 'black',
-    fontWeight: 'bold'
+    justifyContent: "center",
+    alignItems: "center",
+    color: "black",
+    fontWeight: "bold",
   },
   sliderThumb: {
     width: 12,
     height: 12,
-    backgroundColor: '#EA5A00',
-    borderRadius: 10 / 2
+    backgroundColor: "#EA5A00",
+    borderRadius: 10 / 2,
   },
   sliderTrack: {
     height: 2,
 
-    shadowColor: '#ccc',
+    shadowColor: "#ccc",
     shadowOffset: {
       width: 0,
-      height: 0
+      height: 0,
     },
     shadowRadius: 5,
-    shadowOpacity: 5
+    shadowOpacity: 5,
   },
   radioControlsContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
     marginTop: 15,
-    width: '80%'
+    width: "80%",
   },
   bottomSection: {
-    borderTopColor: '#F4EEEA',
+    borderTopColor: "#F4EEEA",
     borderWidth: 1,
-    width: '100%',
-    alignItems: 'center',
-    paddingVertical: 15
+    width: "100%",
+    alignItems: "center",
+    paddingVertical: 15,
   },
 
   flex: {
-    flexDirection: 'row',
+    flexDirection: "row",
     marginTop: 20,
-    height: 60
+    height: 60,
   },
   centeredView: {
     flex: 1,
-    marginTop: 22
+    marginTop: 22,
   },
   modalView: {
     margin: 20,
-    backgroundColor: 'white',
+    backgroundColor: "white",
     borderRadius: 20,
     padding: 35,
-    shadowColor: '#000',
+    shadowColor: "#000",
     shadowOffset: {
       width: 0,
-      height: 2
+      height: 2,
     },
     shadowOpacity: 0.25,
     shadowRadius: 4,
     elevation: 5,
-    maxHeight: Dimensions.get('screen').height - 20
+    maxHeight: Dimensions.get("screen").height - 20,
   },
   button: {
     borderRadius: 20,
     padding: 10,
     elevation: 2,
-    marginTop: 30
+    marginTop: 30,
   },
   buttonOpen: {
-    backgroundColor: '#F194FF'
+    backgroundColor: "#F194FF",
   },
   buttonClose: {
-    backgroundColor: '#2196F3'
+    backgroundColor: "#2196F3",
   },
   textStyle: {
-    color: 'white',
-    fontWeight: 'bold',
-    textAlign: 'center'
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center",
   },
   modalText: {
     marginBottom: 15,
-    textAlign: 'center'
+    textAlign: "center",
   },
   textInput: {
     marginTop: 10,
-    marginBottom: 10
+    marginBottom: 10,
   },
   textInputLarge: {
     marginTop: 10,
     marginBottom: 10,
-    height: 150
+    height: 150,
   },
   showMoreButton: {
     marginLeft: 10,
   },
   showMoreText: {
-    color: 'orange',
-    marginLeft: 5
+    color: "orange",
+    marginLeft: 5,
   },
   podcastHeader: {
-    height: 200
+    height: 200,
   },
   descriptionText: {
     marginTop: 5,
   },
   smallDescription: {
-    marginLeft: 10
+    marginLeft: 10,
   },
   podcastImage: {
     paddingRight: 10,
-    aspectRatio: 1*1,
-    maxHeight: '100%'
+    aspectRatio: 1 * 1,
+    maxHeight: "100%",
   },
   episodeHeader: {
     fontSize: 30,
     marginBottom: 10,
-    color: 'black'
+    color: "black",
   },
   episodeName: {
-    color: 'black',
+    color: "black",
     fontSize: 15,
-    fontWeight: 'bold',
-    marginBottom: 10
+    fontWeight: "bold",
+    marginBottom: 10,
   },
   episodeDivider: {
     height: 2,
-    marginTop: 10
+    marginTop: 10,
   },
   listItem: {
-    height: 80
+    height: 80,
   },
   dropDown: {
-    marginTop: 20, 
+    marginTop: 20,
     marginBottom: 5,
-  }, 
+  },
   scrollView: {
     marginTop: 100,
-  }, 
+  },
   searchBox: {
     marginBottom: 20,
-    borderColor: '#333',
+    borderColor: "#333",
     borderWidth: 1,
     padding: 5,
-}, 
+  },
   image: {
     //flex: 1,
     justifyContent: "center",
-    alignItems: 'center',
+    alignItems: "center",
     height: 210,
     //maxHeight: '100%'
-}, 
+  },
   textAccordion: {
     //textAlign: 'center',
-    fontWeight: 'bold',
+    fontWeight: "bold",
     fontSize: 22,
-  }, 
+  },
   textImage: {
-    textAlign: 'center',
-    fontWeight: 'bold',
+    textAlign: "center",
+    fontWeight: "bold",
     fontSize: 22,
-  }, 
+  },
   buttonSmall: {
-    borderRadius: 20, 
-    width: 135, 
-    height: 45, 
-    alignContent: 'center',
-    justifyContent: 'center',
-    marginTop: 20, 
-    backgroundColor: theme.colors.primary
-  }, 
+    borderRadius: 20,
+    width: 135,
+    height: 45,
+    alignContent: "center",
+    justifyContent: "center",
+    marginTop: 20,
+    backgroundColor: theme.colors.primary,
+  },
   buttonSmallDisabled: {
-    borderRadius: 20, 
-    width: 135, 
-    height: 45, 
-    alignContent: 'center',
-    justifyContent: 'center',
-    marginTop: 20, 
-    backgroundColor: theme.colors.neutral80, opacity: 0.87
-    
-  }, 
+    borderRadius: 20,
+    width: 135,
+    height: 45,
+    alignContent: "center",
+    justifyContent: "center",
+    marginTop: 20,
+    backgroundColor: theme.colors.neutral80,
+    opacity: 0.87,
+  },
   center: {
-    alignContent: 'center',
+    alignContent: "center",
     //justifyContent: 'center',
   },
-  textMediumBody:{
+  textMediumBody: {
     color: theme.colors.onSurface,
     fontSize: 12,
-  }
-})
+    flex: 1,
+  },
+  flexContainer: {
+    flex: 1,
+    flexDirection: "row",
+  },
+  textMediumTitle: {
+    color: theme.colors.onSurface,
+    fontWeight: "bold",
+    fontSize: 22,
+    flex: 1,
+  },
+  runImage: {
+    flex: 1,
+    justifyContent: "flex-start",
+    height: "100%",
+    width: "100%",
+    resizeMode: "contain",
+  },
+  runsContainer: {
+    height: 210,
+  },
+  runsImg: {
+    flex: 1,
+    justifyContent: "space-between",
+    flexDirection: "row",
+  },
+  runsHeader: {
+    color: theme.colors.onSurface,
+    fontWeight: "bold",
+    fontSize: 22,
+    textAlign: "left",
+  },
+  runsBody: {
+    color: theme.colors.onSurface,
+    fontSize: 12,
+    textAlign: "center",
+    paddingTop: 10,
+    paddingRight: 5,
+  },
+});
